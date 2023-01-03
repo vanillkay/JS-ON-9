9277807095e17ec8f60df080de520c9a1b754c9c
Andrew <vanillkay@gmail.com>
2023-01-03 22:34:25 +0200

    Initial

---
+++ lesson-2/meterials.md
@@ -0,0 +1,2 @@
+1. https://uk.javascript.info - Вступ, Основи ( 2.1 - 2.6 ) + задачки
+2. https://naydyonovdanil.gitbook.io/javascript/dlya-izucheniya/javascript/chto-takoe-javascript - Методичка
---
+++ lesson-2/scripts/script.js
@@ -0,0 +1,51 @@
+let message = 'Hello world !'; // string
+let message2 = "Hello world !"; // string
+
+// console.log(message);
+
+
+message = 'Hello from JS';
+
+// console.log(message);
+
+let firstNumber = 22; // number 
+let secondNumber = 45; // number
+
+// console.log(firstNumber, secondNumber)
+
+// console.log('+', firstNumber + secondNumber);
+// console.log('-', secondNumber - firstNumber);
+// console.log('/', secondNumber / firstNumber);
+// console.log('*', firstNumber * secondNumber);
+// console.log('%', secondNumber % firstNumber);
+
+
+// console.log('Before', firstNumber);
+// // firstNumber = firstNumber + 3;
+// firstNumber += 3
+
+// console.log('After', firstNumber)
+
+// console.log(5 + 3 * 2) // 5 + 6
+// console.log((4 - 6) * 2 + 8 / 4) // -2
+
+
+// let string = `Hello world !`;
+
+// console.log('First number is ' + firstNumber + ', Second number is ' + secondNumber)
+// console.log(`First number is ${firstNumber}, Second number is ${secondNumber}`)
+
+
+// let stringNumber = '34asdas';
+
+// console.log(Number(stringNumber)) // V1 
+
+// console.log(+stringNumber) // V2
+
+// console.log(parseInt(stringNumber)) // V2
+
+// console.log(parseFloat(stringNumber))
+
+let result = prompt('Enter the number', 9);
+
+console.log(Number(result))
